a916886235d30fb7d1cc4a301e3daf896f9fbfaa
joseph-musyoki <musyokijoseph42@gmail.com>
2025-02-26 20:57:24 -0800

    testing something

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Hello from './assets/components/Hello'
+
+function App() {
+
+
+  return (
+    <>
+     <Hello/>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Hello.jsx
@@ -0,0 +1,7 @@
+
+
+export default function Hello(){
+    return <div className="hey">
+        <h1>hey there how are you</h1>
+    </div>
+}
